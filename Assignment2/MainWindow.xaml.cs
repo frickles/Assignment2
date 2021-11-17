ca43bdf98c1eeb04f5580c275e457dc8216223b6
Frida Lindberg <fridalindberg__@hotmail.com>
2021-11-17 20:18:44 +0100

    Working Code

--- Assignment2/MainWindow.xaml.cs
+++ Assignment2/MainWindow.xaml.cs
@@ -22,16 +22,13 @@ namespace Assignment2
     {
         private Thickness spacing = new Thickness(5);
         private HttpClient http = new HttpClient();
-        // We will need these as instance variables to access in event handlers.
         private TextBox addFeedTextBox;
         private Button addFeedButton;
         private ComboBox selectFeedComboBox;
         private Button loadArticlesButton;
         private StackPanel articlePanel;
         private List<string> urls = new List<string>();
-        // Do we need this list here?
-        //private List<string> urls = new List<string>();
-        //private List<string> articles = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -40,7 +37,6 @@ namespace Assignment2
 
         private void Start()
         {
-            // Added region for convenience of hiding this section
             #region Design
             // Window options
             Title = "Feed Reader";
@@ -134,57 +130,61 @@ namespace Assignment2
             Grid.SetRow(articlePanel, 2);
             Grid.SetColumnSpan(articlePanel, 3);
             #endregion
-
-            // These are just placeholders.
-            // Replace them with your own code that shows actual articles.
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    var articlePlaceholder = new StackPanel
-            //    {
-            //        Orientation = Orientation.Vertical,
-            //        Margin = spacing
-            //    };
-            //    articlePanel.Children.Add(articlePlaceholder);
-
-            //    var articleTitle = new TextBlock
-            //    {
-            //        Text = "2021-01-02 12:34 - Placeholder for an actual article title #" + (i + 1),
-            //        FontWeight = FontWeights.Bold,
-            //        TextTrimming = TextTrimming.CharacterEllipsis
-            //    };
-            //    articlePlaceholder.Children.Add(articleTitle);
-
-            //    var articleWebsite = new TextBlock
-            //    {
-            //        Text = "Website name #" + (i + 1)
-            //    };
-            //    articlePlaceholder.Children.Add(articleWebsite);
-            //}
         }
 
-        // Example/testmethods
         private async void LoadArticles(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show("This button should load 5 articles");
             loadArticlesButton.IsEnabled = false;
-            //var article = new Article(); 
-            //string text = selectFeedComboBox.SelectedIndex;
-            foreach (var url in urls)
+
+            if (selectFeedComboBox.SelectedIndex == 0)
             {
-                await LoadDocumentAsync(url);
-                var document = XDocument.Load(url);
-                string[] allTitles = document.Descendants("item").Select(t => t.Value).ToArray();
+                foreach (var url in urls)
                 {
+                    for (int i = 1; i < 5; i++)
+                    {
+                        var articlePlaceholder = new StackPanel
+                        {
+                            Orientation = Orientation.Vertical,
+                            Margin = spacing
+                        };
+                        articlePanel.Children.Add(articlePlaceholder);
+
+                        var articleTitle = new TextBlock
+                        {
+                            Text = "2021-01-02 12:34 - Placeholder for an actual article title #" + (i + 1),
+                            FontWeight = FontWeights.Bold,
+                            TextTrimming = TextTrimming.CharacterEllipsis
+                        };
+                        articlePlaceholder.Children.Add(articleTitle);
+
+                        var articleWebsite = new TextBlock
+                        {
+                            Text = "Website name #" + (i + 1)
+                        };
+                        articlePlaceholder.Children.Add(articleWebsite);
+                    }
+                }
+            }
+
+            else
+            {
+                articlePanel.Children.Clear();
+                foreach (var url in urls)
+                {
+                    await LoadDocumentAsync(url);
+                    var document = XDocument.Load(url);
+                    //string[] allTitles = document.Descendants("item").Select(t => t.Value).ToArray();
+
                     foreach (var item in document.Descendants("item"))
                     {
                         string title = item.Descendants("title").First().Value;
                         DateTime year = DateTime.Parse(item.Descendants("pubDate").First().Value);
-                        Title = title;
+
                         for (int i = 0; i < 5; i++)
                         {
                             var articlePlaceholder = new StackPanel
                             {
-
+                                Orientation = Orientation.Vertical,
                                 Margin = spacing
                             };
                             articlePanel.Children.Add(articlePlaceholder);
@@ -192,8 +192,7 @@ namespace Assignment2
                             var articleTitle = new TextBlock
                             {
                                 //Text = "2021-01-02 12:34 - Placeholder for an actual article title #" + (i + 1),
-                                Text = Title,
-
+                                Text = title + " #" + (i + 1),
                                 FontWeight = FontWeights.Bold,
                                 TextTrimming = TextTrimming.CharacterEllipsis
                             };
@@ -201,92 +200,24 @@ namespace Assignment2
 
                             var articleWebsite = new TextBlock
                             {
-                                Text = "Website name #" + (i + 1)
+                                Text = selectFeedComboBox.Text
                             };
                             articlePlaceholder.Children.Add(articleWebsite);
                         }
+
                     }
                 }
             }
-            //LoadDocumentAsync();
-
-            //if (selectFeedComboBox.SelectedIndex == 0)
-            //{
-            //    foreach (var url in urls)
-            //    {
-            //        for (int i = 0; i < 5; i++)
-            //        {
-            //            var articlePlaceholder = new StackPanel
-            //            {
-
-            //                Margin = spacing
-            //            };
-            //            articlePanel.Children.Add(articlePlaceholder);
-
-            //            var articleTitle = new TextBlock
-            //            {
-            //                Text = "2021-01-02 12:34 - Placeholder for an actual article title #" + (i + 1),
-
-            //                FontWeight = FontWeights.Bold,
-            //                TextTrimming = TextTrimming.CharacterEllipsis
-            //            };
-            //            articlePlaceholder.Children.Add(articleTitle);
-
-            //            var articleWebsite = new TextBlock
-            //            {
-            //                Text = "Website name #" + (i + 1)
-            //            };
-            //            articlePlaceholder.Children.Add(articleWebsite);
-            //        }
-            //    }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Vad du vill");
-            //}
-
-
-            //var articles = new List<string>();
-            //        foreach (var item in document.Descendants("item"))
-            //        {
-            //            string title = item.Descendants("title").First().Value;
-            //            DateTime year = DateTime.Parse(item.Descendants("pubDate").First().Value);
-            //            articles.Add(title + " " + year);
-            //        }
-            //var document = XDocument.Load("Example.xml");
-
-            //// Get the title of the first movie as a string.
-            //string firstTitle = document.Descendants("title").First().Value;
-
-            //// Get all titles as an array of strings.
-            //string[] allTitles = document.Descendants("title").Select(t => t.Value).ToArray();
-
-            //// We can loop over Descendants with foreach.
-            //var eightiesTitles = new List<string>();
-            //foreach (var movie in document.Descendants("movie"))
-            //{
-            //    string title = movie.Descendants("title").First().Value;
-            //    int year = int.Parse(movie.Descendants("year").First().Value);
-            //    if (year >= 1980 && year < 1990)
-            //    {
-            //        eightiesTitles.Add(title);
-            //    }
-            //}
-
             loadArticlesButton.IsEnabled = true;
-
-            // We should use the current added url here to list 5 articles from it in the interface/GUI
         }
 
-        // Example/testmethods
         private async void AddFeed(object sender, RoutedEventArgs e)
         {
-            // Button is disabled when it is clicked
             addFeedButton.IsEnabled = false;
             string text = addFeedTextBox.Text;
 
             await LoadDocumentAsync(text);
-            // Button is active again after the delay
+
             addFeedButton.IsEnabled = true;
             ////https://www.comingsoon.net/feed
             ////https://www.cinemablend.com/rss/topic/news/movies
@@ -297,7 +228,6 @@ namespace Assignment2
             }
             else
             {
-                // Clears the container after we have clicked the button and added them to the combobox
                 addFeedTextBox.Clear();
 
                 var document = XDocument.Load(text);
@@ -307,12 +237,8 @@ namespace Assignment2
                 urls.Add(text);
             }
         }
-
-        // We should use this premade method to simulate the async-time
         private async Task<XDocument> LoadDocumentAsync(string url)
         {
-            // This is just to simulate a slow/large data transfer and make testing easier.
-            // Remove it if you want to.
             await Task.Delay(1000);
             var response = await http.GetAsync(url);
             response.EnsureSuccessStatusCode();
